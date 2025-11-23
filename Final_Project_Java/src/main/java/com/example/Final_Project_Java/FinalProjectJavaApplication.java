98f1219803c939b8b508a4edd3637d8fbf05b37d
HuynhThanhHiep <hiepka6@gmail.com>
2025-11-23 16:41:41 +0700

    day1
    day1

---
+++ Final_Project_Java/src/main/java/com/example/Final_Project_Java/FinalProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Final_Project_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectJavaApplication.class, args);
+	}
+
+}
